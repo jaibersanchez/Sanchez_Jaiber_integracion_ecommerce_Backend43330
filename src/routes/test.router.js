08a9365145d8c0371cb4bdae4200de08b79c6401
jaibersanchez <jaibersanchez.d@hotmail.com>
2023-08-23 20:53:55 -0500

    se sube desafio

---
+++ src/routes/test.router.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+export const testLogger = express.Router();
+
+testLogger.get('/', (req, res) => {
+  try {
+  } catch (error) {
+    req.logger.error(error);
+  }
+  req.logger.debug('Debug');
+  req.logger.http('HTTP');
+  req.logger.info('reportando algo - Informacion');
+  req.logger.warn('algo no tan malo ocurrio');
+  req.logger.error('TODO MAL :(');
+  req.logger.error('Nivel fatal');
+
+  res.send({ message: 'Hola mundo' });
+});
